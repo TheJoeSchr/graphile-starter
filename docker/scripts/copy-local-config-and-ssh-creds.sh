ce325c3a46abbaac53b1aafb4cbaf0a7973344de
JoeSchr <JoeSchr@users.noreply.github.com>
2020-03-29 18:56:17 +0200

    Update copy-local-config-and-ssh-creds.sh
    Got this question: 
    >The thing that is confusing me is that the scripts and volumes all copy config files and create the docker volumes under /root but when i start a bash in the docker session I'm the "node" user. Vscode is also installing the .devcontainer extensions that I specify under /home/node and ignores anything that is in /root I guess I'm mixing something up here
    
    This is fixing that.

--- docker/scripts/copy-local-config-and-ssh-creds.sh
+++ docker/scripts/copy-local-config-and-ssh-creds.sh
@@ -2,27 +2,42 @@
 
 echo "copy local configs"
 
+## SHOULD NOT NEEDED ANYMORE:
+# ssh-agent should be forwared by vscode
+# but sadly still is...
+
 # Copy ssh creed from local home
 # Needed for e.g. git push etc
-mkdir -p /root/.ssh
-cp -r /root/.home-localhost/.ssh/* /root/.ssh
-chmod 600 /root/.ssh/*
+mkdir -p $HOME/.ssh
+cp -r /.home-localhost/.ssh/* $HOME/.ssh
+chmod 600 $HOME/.ssh/*
 
 # Copy gitconfig
-cp /root/.home-localhost/.gitconfig /root/.gitconfig
+rm $HOME/.gitconfig ; cp /.home-localhost/.gitconfig $HOME/
 # Copy bashconfig
-cp /root/.home-localhost/.bashrc /root/.bashrc
+rm $HOME/.bashrc ; cp /.home-localhost/.bashrc $HOME/
 # Copy tmux
-cp /root/.home-localhost/tmux.conf /root/tmux.conf
+rm $HOME/.tmux.conf ; cp /.home-localhost/.tmux.conf $HOME/
 # Copy vimrc for vim & nvim
-mkdir -p /root/.config/nvim
-echo "/root/.vimrc /root/.config/nvim/init.vim" | xargs -n 1 cp -v /root/.home-localhost/.vimrc
+echo "Copying nvim/init.vim"
+mkdir -p $HOME/.config/nvim
+echo "$HOME/.vimrc $HOME/.config/nvim/init.vim" | xargs -n 1 cp -v /.home-localhost/.config/nvim/init.vim
 
-# Copy vim plugins, eg. "https://github.com/junegunn/vim-plug"
-mkdir -p ~/.vim/autoload
-mkdir -p /root/.local/share/nvim/site/autoload
-echo "/root/.vim/autoload/ /root/.local/share/nvim/site/autoload/" | xargs -n 1 cp -r -v /root/.home-localhost/.vim/autoload/*
-echo "/root/.bashrc /root/.gitconfig /root/.ssh/config /root/.vimrc /root/.config/nvim/init.vim" | xargs -n 1 dos2unix
+# dos2unix dotfiles
+echo "$HOME/.bashrc $HOME/.gitconfig $HOME/.ssh/config $HOME/.vimrc $HOME/.config/nvim/init.vim" | xargs -n 1 dos2unix
 
 # Installs (n)vim plugins
 nvim +'PlugInstall --sync' +qa true
+
+# Installs tmux plugin manager
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+# Setting for the new UTF-8 terminal support in TMUX
+touch /usr/share/locale/locale.alias;
+sed -i -e 's/# en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen && \
+    dpkg-reconfigure --frontend=noninteractive locales && \
+    update-locale LANG=en_US.UTF-8
+
+export LC_CTYPE=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+export LANG=en_US.UTF-8
